70701d58f129b2ecd336a482178264f929f605bb
Xunerger <newbeibao@163.com>
2023-06-18 00:28:05 +0800

    session 6.4

--- meadowlark.js
+++ meadowlark.js
@@ -12,22 +12,38 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'))
 
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+    req.query.test === '1';
+    next();
+})
+
 app.get('/', function(req, res){
     res.render('home')
     // res.type('text/plain');
     // res.send('Meadowlark Travel');
 });
 app.get('/about', function(req, res) {
-    res.render('about', { fortune: fortune.getFortune() } );
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
     });
-// app.get('/about', function(req, res){
-//     var randomFortune =
-//     fortunes[Math.floor(Math.random() * fortunes.length)];
-//     res.render('about', { fortune: randomFortune });
-//     // res.render('about')
-//     // res.type('text/plain');
-//     // res.send('About Meadowlark Travel');
-// });
+});
+app.get('/headers', function(req,res){
+    res.set('Content-Type','text/plain');
+    var s = '';
+    for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+    res.send(s);
+    });
+
+// 测试路由
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
+});
+
 // 定制 404 页面
 app.use(function(req, res, next){
     // res.type('text/plain');
