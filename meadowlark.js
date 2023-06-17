b64da003bbbf812c8f39a329c74f72967f25789a
Xunerger <newbeibao@163.com>
2023-06-17 23:20:12 +0800

    Moved 'fortune cookie' functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var fortune = require('./lib/fortune.js');
 
 // 设置 handlebars 视图引擎
 var handlebars = require('express3-handlebars')
@@ -11,27 +12,22 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'))
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-]
-
 app.get('/', function(req, res){
     res.render('home')
     // res.type('text/plain');
     // res.send('Meadowlark Travel');
 });
-app.get('/about', function(req, res){
-    var randomFortune =
-    fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
-    // res.render('about')
-    // res.type('text/plain');
-    // res.send('About Meadowlark Travel');
-});
+app.get('/about', function(req, res) {
+    res.render('about', { fortune: fortune.getFortune() } );
+    });
+// app.get('/about', function(req, res){
+//     var randomFortune =
+//     fortunes[Math.floor(Math.random() * fortunes.length)];
+//     res.render('about', { fortune: randomFortune });
+//     // res.render('about')
+//     // res.type('text/plain');
+//     // res.send('About Meadowlark Travel');
+// });
 // 定制 404 页面
 app.use(function(req, res, next){
     // res.type('text/plain');
